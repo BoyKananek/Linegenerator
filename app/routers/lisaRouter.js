8cd3dc972569eb0cc3144dc2135bd5fd19d9b75a
Kananek Atichatviwat <kananek.ati@hotmail.com>
2016-11-25 17:40:50 +0700

    adding wait until

--- app/routers/lisaRouter.js
+++ app/routers/lisaRouter.js
@@ -5,6 +5,9 @@ var request = require('request');
 var uuid = require('uuid');
 var events = require('events');
 var fs = require('fs');
+var async = require('async');
+var waitUntil = require('wait-until');
+
 var eventEmitter = new events.EventEmitter();
 
 //main content
@@ -27,7 +30,7 @@ var eventEmitter = new events.EventEmitter();
 var rand = uuid.v4();  // uuid
 var currentTime = new Date().getTime(); //currentTime
 var list;
-
+var xml;
 
 var urls = new Array(); // list of url
 var shortlinks = new Array(); // list of shortlink
@@ -46,41 +49,18 @@ var image_re2 = new Array();
 var url_re3 = new Array();
 var title_re3 = new Array();
 var image_re3 = new Array();
+
 router.post('/generateContent', function (req, res) {
     list = req.body.data;
-    /*var rand = uuid.v4();  // uuid
-    var currentTime = new Date().getTime(); //currentTime
-    var list = req.body.data;
 
-    var urls = Array(); // list of url
-    var shortlinks = Array(); // list of shortlink
-    var titles = Array(); // list of the title of the content
-    var categories = Array(); // list of category of the content
-    var images = Array();  //list of image of the content
-    var contents = Array(); //list of content
-
-    //recommended article
-    var url_re1 = Array();
-    var title_re1 = Array();
-    var image_re1 = Array();
-
-    var url_re2 = Array();
-    var title_re2 = Array();
-    var image_re2 = Array();
-
-    var url_re3 = Array();
-    var title_re3 = Array();
-    var image_re3 = Array();
-    */
-    var isDone = false;
-    console.log("Getting each content data");
     for (var i = 0; i < list.length; i++) {
         urls.push(list[i].url);
         url_re1.push(list[i].recommended1);
         url_re2.push(list[i].recommended2);
         url_re3.push(list[i].recommended3);
     }
-    console.log("Grabbing content inside of the each content")
+
+
     for (var i = 0; i < list.length; i++) {
         console.log("Request for content " + i);
         request(urls[i], function (err, res, html) {
@@ -92,8 +72,7 @@ router.post('/generateContent', function (req, res) {
                 }).toArray();
                 var temp = pre_shortlink[0];
                 shortlinks.push(temp.substr(24));
-                console.log(temp.substr(24));
-                console.log(shortlinks[i]);
+
                 //find title
                 var pre_title = $("h1.article-title").map(function () {
                     return $(this).text();
@@ -121,7 +100,7 @@ router.post('/generateContent', function (req, res) {
                 contents.push(pre_content[0].substr(len));
             }
         })
-        //recommended 1
+
         console.log("Request for recommended 1 in content " + i);
         request(url_re1[i], function (err, res, html) {
             if (!err) {
@@ -140,7 +119,8 @@ router.post('/generateContent', function (req, res) {
         });
 
         //recommended2
-        console.log("Request for recommended 3 in content " + i);
+        console.log("Request for recommended 2 in content " + i);
+
         request(url_re2[i], function (err, res, html) {
             if (!err) {
                 var $ = cheerio.load(html);
@@ -159,6 +139,7 @@ router.post('/generateContent', function (req, res) {
 
         //recommended3
         console.log("Request for recommended 3 in content " + i);
+
         request(url_re3[i], function (err, res, html) {
             if (!err) {
                 var $ = cheerio.load(html);
@@ -174,21 +155,31 @@ router.post('/generateContent', function (req, res) {
 
             }
         });
-        
-    }
-    //eventEmitter.emit('generate');
-    res.end('Complete');
-    /*eventEmitter.on('generate',function(){
-        var xml = "<?xml version='1.0' encoding='UTF-8' ?><articles><UUID>";
-        xml += xml + rand;
-        xml += xml + "</UUID><time>"
-        var currentTime = new Date().getTime();
-        xml += xml + currentTime + "</time>";
 
-    });*/
+    }
+
+    waitUntil()
+        .interval(5000)
+        .times(1)
+        .condition(function(){
+            //nothing
+        })
+        .done(function(){
+            eventEmitter.emit('generate');
+            res.end('Complete');
+        });
+    
+    
 })
 
+
 eventEmitter.on('generate', function () {
+    console.log('Call');
+    /*xml = "<?xml version='1.0' encoding='UTF-8' ?><articles><UUID>";
+        xml += xml + rand;
+        xml += xml + "</UUID><time>"
+        xml += xml + currentTime + "</time>"; */
+
     for (var i = 0; i < list.length; i++) {
         console.log("This is the " + i + " article");
         console.log(urls[i]);
@@ -196,7 +187,7 @@ eventEmitter.on('generate', function () {
         console.log(titles[i]);
         console.log(categories[i]);
         console.log(images[i]);
-        console.log(contents[i]);
+        //console.log(contents[i]);
 
         console.log("Recommened Article");
         console.log(url_re1[i]);
@@ -209,7 +200,24 @@ eventEmitter.on('generate', function () {
         console.log(title_re3[i]);
         console.log(image_re3[i]);
     }
-    eventEmitter.removeAllListeners();
+    //clear list
+    //eventEmitter.removeAllListeners();
+    /*urls = [];
+    shortlinks =[]
+    titles = [];
+    categories = [];
+    image = [];
+
+    url_re1 = [];
+    title_re1 = [];
+    image_re1 = [];
+    url_re2 = [];
+    title_re2 =[];
+    image_re2 = [];
+    url_re3 = [];
+    title_re3 = [];
+    image_re3 = []; */
+
 })
 
 
