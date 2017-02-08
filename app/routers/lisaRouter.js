462ff6444cba2da283b72a9f9babd23a8580e286
Kan-anek <kan-anek.a@burda.co.th>
2017-02-08 14:41:46 +0700

    fix bugs for lisa

--- app/routers/lisaRouter.js
+++ app/routers/lisaRouter.js
@@ -81,8 +81,150 @@ router.post('/generateContent', function (req, res) {
         url_re3.push(list[i].recommended3);
     }
 
+    var getMain = function (requestCountMain) {
+        if (requestCountMain === list.length) {
+            return;
+        } else {
+            request(urls[requestCountMain], function (err, res, html) {
+                if (!err) {
+                    var $ = cheerio.load(html);
+                    //find shortlink
+                    var pre_shortlink = $("link[rel='shortlink']").map(function () {
+                        return $(this).attr('href');
+                    }).toArray();
+                    var temp = pre_shortlink[0];
+                    shortlinks.push(temp.substr(24));
 
-    for (var i = 0; i < list.length; i++) {
+                    //find title
+                    var pre_title = $("h1.article-title").map(function () {
+                        return $(this).text();
+                    }).toArray();
+                    console.log(pre_title[0]);
+                    titles.push(pre_title[0]);
+
+                    //find category
+                    var pre_category = $("div.breadcrumb a[rel='category tag']").map(function () {
+                        return $(this).text();
+                    }).toArray();
+                    console.log(pre_category[0]);
+                    if (pre_category[0].includes("&")) {
+                        pre_category[0] = pre_category[0].replace("&", "&amp;");
+                        console.log(pre_category[0]);
+                        categories.push(pre_category[0]);
+                    }
+                    else {
+                        categories.push(pre_category[0]);
+                    }
+
+                    //find image url
+                    var pre_image = $("img.attachment-banner-image").map(function () {
+                        return $(this).attr('src');
+                    }).toArray();
+                    images.push(pre_image[0]);
+
+                    //find content 
+                    var pre_content = $("div.article-detail div.col-md-11").remove("aside.mashsh-container").map(function () {
+                        return $(this).html();
+                    }).toArray();
+                    var content = pre_content[0].split("</aside>");
+                    contents.push(content[1]);
+
+                }
+                return getMain(requestCountMain + 1);
+            })
+            
+        }
+    }
+
+    var getRecommended1 = function (requestCountRe1) {
+        if (requestCountRe1 === list.length) {
+            return;
+        } else {
+            request(url_re1[requestCountRe1], function (err, res, html) {
+                if (!err) {
+                    var $ = cheerio.load(html);
+                    var title = $("h1.article-title").map(function () {
+                        return $(this).text();
+                    }).toArray();
+                    title_re1.push(title[0]);
+
+                    var image = $("img.attachment-banner-image").map(function () {
+                        return $(this).attr('src');
+                    }).toArray();
+                    image_re1.push(image[0]);
+
+                }
+                return getRecommended1(requestCountRe1 + 1);
+            });
+            
+        }
+        
+    }
+
+    var getRecommended2 = function (requestCountRe2) {
+        if (requestCountRe2 === list.length) {
+            return;
+        } else {
+            request(url_re2[requestCountRe2], function (err, res, html) {
+                if (!err) {
+                    var $ = cheerio.load(html);
+                    var title = $("h1.article-title").map(function () {
+                        return $(this).text();
+                    }).toArray();
+                    title_re2.push(title[0]);
+
+                    var image = $("img.attachment-banner-image").map(function () {
+                        return $(this).attr('src');
+                    }).toArray();
+                    image_re2.push(image[0]);
+
+                }
+                return getRecommended2(requestCountRe2 + 1);
+            });
+            
+        }
+    }
+
+    var getRecommended3 = function (requestCountRe3) {
+        if (requestCountRe3 === list.length) {
+            return;
+        } else {
+            request(url_re3[requestCountRe3], function (err, res, html) {
+                if (!err) {
+                    var $ = cheerio.load(html);
+                    var title = $("h1.article-title").map(function () {
+                        return $(this).text();
+                    }).toArray();
+                    title_re3.push(title[0]);
+
+                    var image = $("img.attachment-banner-image").map(function () {
+                        return $(this).attr('src');
+                    }).toArray();
+                    image_re3.push(image[0]);
+
+                }
+                 return getRecommended3(requestCountRe3+1);
+            });
+        }
+    }
+
+    getMain(0);
+    getRecommended1(0);
+    getRecommended2(0);
+    getRecommended3(0);
+
+     waitUntil()
+        .interval(2000)
+        .times(list.length)
+        .condition(function () {
+            //nothing
+        })
+        .done(function () {
+            eventEmitter.emit('generate');
+            res.end('Complete!!!');
+        });
+
+    /*for (var i = 0; i < list.length; i++) {
         console.log("Request for content " + (i + 1));
         request(urls[i], function (err, res, html) {
             if (!err) {
@@ -210,19 +352,32 @@ router.post('/generateContent', function (req, res) {
                 res.end('Complete!!!');
             });
     }
-
+    */
 
 })
 
 
 eventEmitter.on('generate', function () {
+    console.log("main title");
+    console.log(titles);
+    console.log("main url");
+    console.log(urls);
+    console.log("recommended1 url");
+    console.log(url_re1);
+    console.log(title_re1);
+    console.log("recommended2 url");
+    console.log(url_re2);
+    console.log(title_re2);
+    console.log("recommended3 url");
+    console.log(url_re3);
+    console.log(title_re3);
     xml = "<?xml version='1.0' encoding='UTF-8' ?><articles><UUID>";
     xml += rand;
     xml += "</UUID><time>";
     xml += currentTime.toString() + "</time>";
 
     for (var i = 0; i < list.length; i++) {
-        
+
         xml += "<article>";
         xml += "<ID>" + shortlinks[i] + "</ID>";
         xml += "<nativeCountry>TH</nativeCountry><language>th</language><publishCountries><country>TH</country></publishCountries>";
